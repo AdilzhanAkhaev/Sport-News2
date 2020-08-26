954fd74b352147c772d871dadbc6418f007432bd
Adilzhan <Adil02012000@gmail.com>
2020-08-26 18:54:17 +0600

    Initial Commit

---
+++ Sport News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sport News
+//
+//  Created by Adilzhan Akhayev on 8/26/20.
+//  Copyright © 2020 Adilzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
